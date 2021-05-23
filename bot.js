b997dd0ef7dac86985d14e2f4b85fe8f8396742e
ʀɛռɢǟʀツ <80170298+rengar02@users.noreply.github.com>
2021-05-23 19:42:39 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,275 @@
+const Discord = require('discord.js');// rengar
+const client = new Discord.Client();// rengar
+const ayarlar = require('./ayarlar.json');// rengar
+const chalk = require('chalk');// rengar
+const moment = require('moment');// rengar
+var Jimp = require('jimp');// rengar
+const { Client, Util } = require('discord.js');// rengar
+const fs = require('fs');// rengar
+const db = require('quick.db');// rengar
+const express = require('express');//
+require('./util/eventLoader.js')(client);// rengar
+const path = require('path');// rengar
+const snekfetch = require('snekfetch');// rengar
+const ms = require('ms');// rengar
+const tags = require('common-tags')
+// rengar
+
+var prefix = ayarlar.prefix;// rengar
+
+const log = message => {//
+    console.log(`${message}`);//
+};
+
+client.commands = new Discord.Collection();//
+client.aliases = new Discord.Collection();//
+fs.readdir('./komutlar/', (err, files) => {//
+    if (err) console.error(err);//
+    log(`‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
+    ${files.length} komut yüklenecek.
+‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒`);//
+    files.forEach(f => {//
+        let props = require(`./komutlar/${f}`);//
+        log(`[KOMUT] | ${props.help.name} Eklendi.`);//
+        client.commands.set(props.help.name, props);//
+        props.conf.aliases.forEach(alias => {//
+            client.aliases.set(alias, props.help.name);//
+        });
+    });
+});
+
+
+
+
+client.reload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.load = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+
+
+client.unload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.elevation = message => {
+    if (!message.guild) {
+        return;
+    }
+
+    let permlvl = 0;
+    if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+    if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+    if (message.author.id === ayarlar.sahip) permlvl = 4;
+    return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+client.on('warn', e => {
+    console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+client.on('error', e => {
+    console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+
+client.login(process.env.token);
+
+//-------------------------------------------------sea----------------------------ase----------------------------------------\\
+
+client.on("message", message => {
+    if(message.content.toLowerCase() == "sa") 
+    return message.channel.send(`${message.author}, Aleyküm Selam.`)
+});
+
+client.on("message", message => {
+    if(message.content.toLowerCase() == "selam") 
+    return message.channel.send(`${message.author}, Sanada Selam hoşgeldin.`)
+});
+
+client.on("message", message => {
+    if(message.content.toLowerCase() == "merhaba") 
+    return message.channel.send(`${message.author}, Merhaba Kanka.`)
+});
+
+client.on("message", message => {
+    if(message.content.toLowerCase() == "s.a") 
+    return message.channel.send(`${message.author}, Aleyküm Selam.`)
+});
+
+client.on("message", message => {
+    if(message.content.toLowerCase() == "mrb") 
+    return message.channel.send(`${message.author}, Sanada mrb.`)
+});
+
+client.on("message", message => {
+    if(message.content.toLowerCase() == "slm") 
+    return message.channel.send(`${message.author}, Sanada selamm hoşgeldin.`)
+});
+
+//-------------------------------------------------sea----------------------------ase----------------------------------------\\
+
+//--------------------------------------------------tag----------------------------tag----------------------------------------\\
+client.on("message", message => {
+    if(message.content.toLowerCase() == "!tag") 
+    return message.channel.send(`Tag`)
+});
+
+client.on("message", message => {
+    if(message.content.toLowerCase() == "tag") 
+    return message.channel.send(`Tag`)
+});
+
+//---------------------------------------------------afk-------------------------------------------------------\\
+
+
+client.on("message" , async msg => {
+  
+  if(!msg.guild) return;
+  if(msg.content.startsWith(ayarlar.prefix+"afk")) return; 
+  
+  let afk = msg.mentions.users.first()
+  
+  const kisi = db.fetch(`afkid_${msg.author.id}_${msg.guild.id}`)
+  
+  const isim = db.fetch(`afkAd_${msg.author.id}_${msg.guild.id}`)
+ if(afk){
+   const sebep = db.fetch(`afkSebep_${afk.id}_${msg.guild.id}`)
+   const kisi3 = db.fetch(`afkid_${afk.id}_${msg.guild.id}`)
+   if(msg.content.includes(kisi3)){
+
+       msg.channel.send(new Discord.MessageEmbed().setColor('BLACK').setDescription(`<@` + msg.author.id + `> Etiketlediğiniz Kişi Afk \nSebep : ${sebep}`))
+   }
+ }
+  if(msg.author.id === kisi){
+
+       msg.channel.send(new Discord.MessageEmbed().setColor('BLACK').setDescription(`<@${kisi}> Başarıyla Afk Modundan Çıktınız`))
+   db.delete(`afkSebep_${msg.author.id}_${msg.guild.id}`)
+   db.delete(`afkid_${msg.author.id}_${msg.guild.id}`)
+   db.delete(`afkAd_${msg.author.id}_${msg.guild.id}`)
+    msg.member.setNickname(isim)
+    
+  }
+  
+});
+//--------------------------------------------------------afk--------------------------------------------------\\
+
+//------------------------------------------------------kufur------------------------------\\
+
+client.on("message", async msg => {
+  
+  
+ const i = await db.fetch(`kufur_${msg.guild.id}`)
+    if (i == "acik") {
+        const kufur = ["oç", "amk", "ananı sikiyim", "ananıskm", "piç", "amk", "amsk", "sikim", "sikiyim", "orospu çocuğu", "piç kurusu", "kahpe", "orospu", "mal", "sik", "yarrak", "am", "amcık", "amık", "yarram", "sikimi ye", "mk", "mq", "aq", "ak", "amq",];
+        if (kufur.some(word => msg.content.includes(word))) {
+          try {
+            if (!msg.member.hasPermission("BAN_MEMBERS")) {
+                  msg.delete();
+                          
+                      return msg.channel.send(new Discord.MessageEmbed().setDescription(`${msg.author} Bu sunucuda küfür filtresi etkin.`).setColor('0x800d0d').setAuthor(msg.member.displayName, msg.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+  
+            }              
+          } catch(err) {
+            console.log(err);
+          }
+        }
+    }
+    if (!i) return;
+});
+
+client.on("messageUpdate", (oldMessage, newMessage) => {
+  
+  
+ const i = db.fetch(`${oldMessage.guild.id}.kufur`)
+    if (i) {
+        const kufur = ["oç", "amk", "ananı sikiyim", "ananıskm", "piç", "amk", "amsk", "sikim", "sikiyim", "orospu çocuğu", "piç kurusu", "kahpe", "orospu", "mal", "sik", "yarrak", "am", "amcık", "amık", "yarram", "sikimi ye", "mk", "mq", "aq", "ak", "amq","amguard","seksüel","sekssüel"];
+        if (kufur.some(word => newMessage.content.includes(word))) {
+          try {
+            if (!oldMessage.member.hasPermission("BAN_MEMBERS")) {
+                  oldMessage.delete();
+                          
+                      return oldMessage.channel.send(new Discord.MessageEmbed().setDescription(`${oldMessage.author} Bu sunucuda küfür filtresi etkin.`).setColor('0x800d0d').setAuthor(oldMessage.member.displayName, oldMessage.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+  
+            }              
+          } catch(err) {
+            console.log(err);
+          }
+        }
+    }
+    if (!i) return;
+});
+
+
+//-------------------------------------------------------kufur-----------------------------\\
+
+//----------------------------------------reklam--------------------------------------------\\
+
+client.on("message", msg => {
+ if(!db.has(`reklam_${msg.guild.id}`)) return;
+        const reklam = [".com", ".net", ".xyz", ".tk", ".pw", ".io", ".me", ".gg", "www.", "https", "http", ".gl", ".org", ".com.tr", ".biz", "net", ".rf.gd", ".az", ".party", "discord.gg",];
+        if (reklam.some(word => msg.content.includes(word))) {
+          try {
+            if (!msg.member.hasPermission("BAN_MEMBERS")) {
+                  msg.delete();
+                    return msg.channel.send(new Discord.MessageEmbed().setDescription(`${msg.author} Bu sunucuda reklam koruması etkin.`).setColor('0x800d0d').setAuthor(msg.member.displayName, msg.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+  
+ 
+  msg.delete(3000);                              
+ 
+            }              
+          } catch(err) {
+            console.log(err);
+          }
+        }
+    });
+
+//--------------------------------------------------reklam----------------------------------\\
+
+client.on("message",message=>{
+    if(message.content==`<@!${client.user.id}>`) return message.channel.send(`Prefixim : **${prefix}**`);
+})
